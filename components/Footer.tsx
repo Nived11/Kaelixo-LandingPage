7df5041c80ee33a64ac7b9416ad8cec8224e2ff1
Nived11 <nivedsuresh5@gmail.com>
2026-08-10 18:04:03 +0530

    icon

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -1,7 +1,7 @@
 export default function Footer() {
   return (
     <footer className="py-8 text-center text-sm text-[#aab2c7] border-t border-[rgba(255,255,255,0.1)]">
-      <p>© {new Date().getFullYear()} Viceversa. All rights reserved.</p>
+      <p>© {new Date().getFullYear()} Kaelixo. All rights reserved.</p>
     </footer>
   );
 }
